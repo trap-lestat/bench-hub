a3adf46f4688a2b28f3f78953493e0f216b8cc8e
trap-lestat <trap454592733@gmail.com>
2026-01-19 23:50:40 +0800

    前端增加未登陆拦截

--- web/src/lib/api.js
+++ web/src/lib/api.js
@@ -13,4 +13,18 @@ api.interceptors.request.use((config) => {
   return config
 })
 
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    const status = error?.response?.status
+    if (status === 401) {
+      localStorage.removeItem('access_token')
+      if (!window.location.pathname.startsWith('/login')) {
+        window.location.href = '/login'
+      }
+    }
+    return Promise.reject(error)
+  }
+)
+
 export default api
